15c44ea831310c6c45ebe59416756f93f7053c59
stamigos <oldtigersvoice@gmail.com>
2015-07-23 15:49:35 +0300

    accept_tos field

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, BooleanField, validators
 
 
 class RegistrationForm(Form):
     email = StringField('Email', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=35)])
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired(u'You must accept our Terms of Service')])
