f0eb743d6cbf9b017034de4ae091aae8925782da
stamigos <oldtigersvoice@gmail.com>
2015-07-28 15:32:13 +0300

    removed accounts to my_trio

--- accounts/forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, StringField, BooleanField, validators
-
-
-class RegistrationForm(Form):
-    email = StringField('Email', [validators.DataRequired(), validators.Email(), validators.Length(min=6, max=35)])
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired(u'You must accept our Terms of Service')])
--- my_trio/__init__.py
+++ my_trio/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_recaptcha import ReCaptcha
 from flask.ext.mail import Mail
 from flask.ext.babel import Babel
 
 from config import GOOGLE_KEY, GOOGLE_SECRET_KEY
+
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_object('config')
@@ -12,6 +13,6 @@ mail = Mail(app)
 babel = Babel(app)
 
 recaptcha.init_app(app, GOOGLE_KEY, GOOGLE_SECRET_KEY)
-from accounts.views import register_page
+from my_trio.accounts.views import register_page
 
 app.register_blueprint(register_page)
